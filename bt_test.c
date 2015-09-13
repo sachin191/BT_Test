a2dcb4b3b54300d7fc3e56595d2ece8ac55d8a83
Sachin Sane <sachin191vex@gmail.com>
2015-09-13 05:49:18 -0700

    Initial commit

---
+++ bt_test.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+
+int main(int argc, char **argv)
+{
+    inquiry_info *ii = NULL;
+    int max_rsp, num_rsp;
+    int dev_id, sock, len, flags;
+    int i;
+    char addr[19] = { 0 };
+    char name[248] = { 0 };
+
+    printf( "Opening device...\n" );
+    dev_id = hci_get_route(NULL);
+    sock = hci_open_dev( dev_id );
+    if (dev_id < 0 || sock < 0) {
+        perror("opening socket");
+        exit(1);
+    }
+
+    len  = 8;
+    max_rsp = 255;
+    flags = IREQ_CACHE_FLUSH;
+    ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
+    
+    printf( "Enquiring device...\n" );
+    num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
+    if( num_rsp < 0 ) perror("hci_inquiry");
+
+    printf( "Trying to get names of %d devices...\n", num_rsp );
+    for (i = 0; i < num_rsp; i++) {
+        printf("Device %d info:\n", i );
+        ba2str(&(ii+i)->bdaddr, addr);
+        memset(name, 0, sizeof(name));
+        if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), 
+            name, 0) < 0)
+        strcpy(name, "[unknown]");
+        printf("%s  %s\n", addr, name);
+        printf("Device done.\n", i );
+    }
+
+    free( ii );
+    close( sock );
+    return 0;
+}
+
---
+++ bt_test.py
@@ -0,0 +1,14 @@
+from bluetooth import *
+
+target_name = "My Phone" 
+target_address = None
+
+nearby_devices = discover_devices()
+for address in nearby_devices:
+	if target_name == lookup_name( address ):
+		target_address = address
+		break
+if target_address is not None:
+	print "found target bluetooth device with address ", target_address 
+else: 
+	print "could not find target bluetooth device nearby"
